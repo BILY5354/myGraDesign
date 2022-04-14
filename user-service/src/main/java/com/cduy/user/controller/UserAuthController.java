9d8b1ed48ef740d1a175ad8ad330a04ab817c128
BILY5354 <2587547190@qq.com>
2022-04-14 22:31:12 +0800

    completed sp-cl 04

--- user-service/src/main/java/com/cduy/user/controller/UserAuthController.java
+++ user-service/src/main/java/com/cduy/user/controller/UserAuthController.java
@@ -1,15 +1,20 @@
 package com.cduy.user.controller;
 
+import com.cduy.user.config.PatternProperties;
 import com.cduy.user.domain.UserAuth;
 import com.cduy.user.service.UserAuthService;
 import com.cduy.user.vo.Result;
 import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 /**
@@ -18,14 +23,33 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/users")
+//@RefreshScope//实现nacos配置热更新 第一种方式，第二种在config.PatternProperties
 public class UserAuthController {
 
     @Autowired
     private UserAuthService userAuthService;
 
+    /*******START 配置热更新*******/
+
+    //第一种 获取配置管理的配置 在nacos上的
+    /*@Value("${pattern.dateformat}")
+    private String dateformat;*/
+
+    //第二种
+    @Autowired
+    private PatternProperties properties;
+
+    @GetMapping("now")
+    public String now() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(properties.getDateformat()));
+    }
+
+    /*******END 配置热更新*******/
+
+
     /**
      * 查看所有用户
-     * */
+     */
     @GetMapping
     public Result<List<UserAuth>> getAll() {
         System.out.println("get all users");
@@ -34,9 +58,9 @@ public class UserAuthController {
 
     /**
      * 根据id查特定用户
-     * */
+     */
     @GetMapping("/{userId}")
-    public Result getUserById(@PathVariable("userId")Integer userId) {
+    public Result getUserById(@PathVariable("userId") Integer userId) {
         return Result.ok(userAuthService.getById(userId));
     }
 
